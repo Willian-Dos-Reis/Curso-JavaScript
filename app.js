694f9313ca645dd2ba052c82c0a7e77713e8b28a
Willian-Dos-Reis <106329846+Willian-Dos-Reis@users.noreply.github.com>
2022-06-14 19:59:31 -0300

    nova aula

---
+++ app.js
@@ -0,0 +1,9 @@
+//colocando frases no console. log
+console.log("oi rapaz")
+
+
+
+// colocando a infromação do id no conscole ,log 
+console.log(
+    document.getElementById("titulo").innerHTML)
+

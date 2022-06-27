b94a24438c13509f01cb7a0694c89bd2a19e06ee
Willian-Dos-Reis <106329846+Willian-Dos-Reis@users.noreply.github.com>
2022-06-27 01:29:29 -0300

    attt

---
+++ Operadores/Aritimetico.js
@@ -0,0 +1,32 @@
+
+        /* 
+        para calculos aritimeticos 
+            5 + 2 = 7
+            5 - 2 = 3
+            5 * 2 = 10
+            5 / 2 = 2,5
+            5 % 2 = 1 (resto da divisao)
+            5 ** 2 = 25 (poetencia)
+
+        para adribuicao
+            var n1 = 5;
+            var n2 = 2;
+            var soma = n1 + n2 
+
+
+        para incremento 
+          var nota = 5;
+
+          x = x + 1  // 6
+          x = x - 1 // 4
+
+
+
+
+        
+
+        */
+
+
+        
+  

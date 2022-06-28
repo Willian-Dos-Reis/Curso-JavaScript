815ec3bd23ff2e08b2763625edd526797520076a
Willian-Dos-Reis <106329846+Willian-Dos-Reis@users.noreply.github.com>
2022-06-27 22:41:41 -0300

    atualizacao da aula

---
+++ Operadores/todos.js
@@ -0,0 +1,80 @@
+
+        /* 
+        para calculos aritimeticos 
+            5 + 2 = 7
+            5 - 2 = 3
+            5 * 2 = 10
+            5 / 2 = 2,5
+            5 % 2 = 1 (resto da divisao)
+            5 ** 2 = 25 (poetencia)
+
+        para adribuicao
+        atribuir um valor 
+            var n1 = 5;
+            var n2 = 2;
+            var soma = n1 + n2 
+
+
+        para incremento 
+        incrimentar um valor 1
+          var nota = 5;
+
+          x = x + 1  // 6
+          x = x - 1 // 4
+
+
+
+        relacionais 
+        relacionar um valor com outro ou um conceito com outro informando se é verdade ou mentira 
+         
+        5 > 2 = true
+        5 < 2 = false
+        5 == 5 = true (esse é se é igual)
+        5 != 5 = false (esse é diferente)
+        5 === "5" = false (se é identico)
+
+
+        logicos 
+        ! nao pode ser 
+        && (e) duas precisam ser true)
+        || (ou) somente uma precisa ser true 
+
+        ternario 
+        teste-logico ? treu : false
+        
+
+
+        ordem de precedencia 
+        1- aritimeticos
+        () ** /
+
+        2- relacionais 
+        > < =  
+
+        3 - logicos 
+         ! (nao)
+         && (é)
+         || (ou)
+
+
+ex
+
+idade >= && idade<=17 
+a iade esta entre 15 e 17 anos ?
+
+estado == 'rj' || estado == 'sp'
+p estado é rio de janeiro ou sao paulo
+
+salario > 1500 && sexo !='M'
+o salrio é acima de 1500 e nao é um homem 
+
+
+        
+
+        */
+
+          
+
+
+        
+  

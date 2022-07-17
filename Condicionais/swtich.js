bbc723a18aea170b5670e4aa4c0b129b7a5fdf38
Willian-Dos-Reis <106329846+Willian-Dos-Reis@users.noreply.github.com>
2022-07-17 18:25:44 -0300

    att

---
+++ Condicionais/swtich.js
@@ -0,0 +1,17 @@
+const compras = 'melancia';
+switch (compras) {
+  case 'laranja':
+    console.log('laranja é $0.59 dolares.');
+    break;
+
+  case 'Mangoes': ('mangoes é 2R$')
+    break
+
+  case 'Papayas':
+    console.log('Mangoes and papayas are $2.79 a pound.');
+    // expected output: "Mangoes and papayas are $2.79 a pound."
+    break;
+
+  default:
+    console.log(`desculpe mas ${compras} nao tem .`);
+}

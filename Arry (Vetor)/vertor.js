04edbc01e566588299f9f9d4f8e4f878140a3aa2
Willian-Dos-Reis <106329846+Willian-Dos-Reis@users.noreply.github.com>
2022-07-26 22:20:36 -0300

    att

--- Arry (Vetor)/vertor.js
+++ Arry (Vetor)/vertor.js
@@ -1,3 +1,10 @@
  let num = [4 ,7 ,5 ,6]
 
-console.log(num.sort())
+ console.log(num[1]) //exiba da varivael num a posicao 1 
+
+ console.log(`ela tem o comprimento de ${num.length} elementos`) //quantidade de elementos 
+
+ console.log(num[5]= 9) // adicione na 5 posicao o valor 9 
+ console.log(num.push(13)) //adicione no final o elemento 13
+
+ console.log(num.sort()) // coloque em ordem numerica
